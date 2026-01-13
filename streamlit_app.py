c69e2e776d6f72f1cb7f60ab0a262dc8cf68c087
maniprasadvs <maniprasad_vs@yahoo.com>
2026-01-13 11:12:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,14 @@ pd_df = my_dataframe.to_pandas()
 
 catalog_list = st.selectbox("Choose Color", pd_df['COLOR_OR_STYLE'], index=None,)
 
-st.write(catalog_list)
 fileName = pd_df.loc[pd_df['COLOR_OR_STYLE'] == catalog_list, 'FILE_NAME'].iloc[0]
 fileimage = pd_df.loc[pd_df['COLOR_OR_STYLE'] == catalog_list, 'FILE_URL'].iloc[0]
+img = Image.open(fileimage)
+st.image(img)
 price = pd_df.loc[pd_df['COLOR_OR_STYLE'] == catalog_list, 'PRICE'].iloc[0]
 size = pd_df.loc[pd_df['COLOR_OR_STYLE'] == catalog_list, 'SIZE_LIST'].iloc[0]
 desc = pd_df.loc[pd_df['COLOR_OR_STYLE'] == catalog_list, 'UPSELL_PRODUCT_DESC'].iloc[0]
 st.write('Price: ', price)
 st.write('Size Avaibale:', size)
+st.write(Desc)
 
